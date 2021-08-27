7d4a572746a6c52b3cf2f4811199f895ff9118ad
organicchiaseed <andersonquoclam@gmail.com>
2021-08-27 17:48:28 +1000

    Update scripts.js
    Set up handleRangeUpdate().

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -18,8 +18,11 @@ function updateButton() {
   }
 
 function skip() {
-    console.log(this.dataset.skip);
     video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
 }
 
 video.addEventListener('click', togglePlay);
@@ -28,3 +31,5 @@ video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
