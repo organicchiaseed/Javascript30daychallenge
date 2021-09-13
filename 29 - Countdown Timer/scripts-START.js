a8f13224bc3b04529aa186eff84ab22db23a0d98
organicchiaseed <andersonquoclam@gmail.com>
2021-09-13 21:28:11 +1000

    Update scripts-START.js
    Work on startTimer().

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,6 +1,7 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelector('[data-time]');
 
 function timer(seconds);
     const now = Date.now();
@@ -34,3 +35,10 @@ function displayEndTime(timestamp) {
     const minutes = end.getMinutes();
     endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+  }
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
