4c6cced1a3ba668d13626a1f750d5f511cb66e34
organicchiaseed <andersonquoclam@gmail.com>
2021-09-13 21:15:53 +1000

    Update scripts-START.js
    Set displayTimeLeft().

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,8 +1,10 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
 
 function timer(seconds);
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -10,5 +12,15 @@ function timer(seconds);
             clearInterval(countdown)
             return;
         }
+        displayTimeLeft(secondsLeft);
     }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${
+        remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
 }
